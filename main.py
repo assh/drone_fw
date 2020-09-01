00e00737a680f2795c3a52e60f75857e22a04698
Asish <asish.panda13@gmail.com>
2020-09-01 11:03:22 +0400

    Added database connection

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException, Command
 import time
 import socket
 import exceptions
 import math
 import argparse
+import psycopg2
+from pymavlink import mavutil
+import os
 
-
+manual = []
+auto = []
+next_date = None
+next_time = None
 def connectDrone():
 
     parser = argparse.ArgumentParser(description='commands')
@@ -18,8 +24,72 @@ def connectDrone():
         sitl = dronekit_sitl.start_default()
         connection_string = sitl.connection_string()
 
-    vehicle = connect(connection_string, wait_ready=True)
+    vehicle = connect(connection_string, baud=57600, wait_ready=True)
     return vehicle
 
 
-vehicle = connectDrone()
+def takeoff(targetH):
+    while vehicle.is_armable != True:
+        print("Waiting for Vehicle to become Armable")
+        time.sleep(1)
+    print("Vehicle is now Armable")
+
+    vehicle.mode = VehicleMode("GUIDED")
+
+    while vehicle.mode != 'GUIDED':
+        print("Waiting for drone to enter GUIDED flight mode")
+        time.sleep(1)
+
+    print("Vehicle is now in GUIDED mode")
+
+    vehicle.armed = True
+
+    while vehicle.armed==False:
+        print("Waiting for vehicle to become armed")
+        time.sleep(1)
+    print("Vehicle is armed")
+
+    vehicle.simple_takeoff(targetH)
+
+    while True:
+        print("Current Altitude: %d"%vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt >= 0.95*targetH:
+            break
+        time.sleep(1)
+    print("Target Altitude reached")
+    return None
+
+
+
+def connectDB():
+    con = psycopg2.connect(database=os.environ.get('DB_NAME'), user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'),
+                       host=os.environ.get('DB_HOST'), port=os.environ.get('DB_PORT'))
+
+    curr = con.cursor()
+    return curr
+
+
+# vehicle = connectDrone()
+cursor  = connectDB()
+exe = """SELECT mission_id,date, "time", launch_mode FROM public.accounts_mission WHERE vda = 'UAE-DR-0001' AND mission_status = 'On Schedule' """
+cursor.execute(exe)
+tmplist = cursor.fetchall()
+for i in tmplist:
+    if (i[3] == 'AUTO'):
+        auto.append(i[0])
+    else:
+        manual.append(i[0])
+print(auto,manual)
+# vehicle.wait_ready('autopilot_version')
+# print(vehicle.is_armable)
+# while vehicle.is_armable != True :
+    # print("waiting for Arming")
+    # time.sleep(1)
+# print("Vehicle now armable")
+# vehicle.mode = VehicleMode("GUIDED")
+# while vehicle.mode != 'GUIDED':
+    # print("Waiting")
+    # time.sleep(1)
+# print(vehicle.mode)
+# vehicle.close()
+
